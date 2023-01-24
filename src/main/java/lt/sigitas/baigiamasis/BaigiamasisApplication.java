3f05e1e382149397a1ab7ca67feb60887a8f96ce
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-24 11:58:26 +0200

    Initial

---
+++ src/main/java/lt/sigitas/baigiamasis/BaigiamasisApplication.java
@@ -0,0 +1,13 @@
+package lt.sigitas.baigiamasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/sigitas/baigiamasis/BaigiamasisApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.sigitas.baigiamasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigiamasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
