e51fed517d03889a258272cd3be6df6e0d20197b
Dailius_VCS <dailiusvcs@gmail.com>
2023-01-25 13:29:23 +0200

    Add insert script into sql_commands.sql

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,1 +1,13 @@
 CREATE SCHEMA nutrientscounter;
+
+DROP TABLE dishes,ingredients;
+
+SELECT * FROM nutrientscounter.ingredients;
+
+
+INSERT INTO ingredients (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`) VALUES (1,'Grikių miltai','Miltai',100,'g.',70.6,12.6);
+INSERT INTO `ingredients` (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`)  VALUES (2,'Migdolų miltai','Miltai',100,'g.',16.2,26.2);
+INSERT INTO `ingredients` (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`) VALUES (3,'Kokosų miltai','Miltai',100,'g.',60,20);
+INSERT INTO `ingredients` (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`)  VALUES (4,'Vištos kiaušinis','Kiaušiniai',50,'g.',0.48,6.24);
+INSERT INTO `ingredients` (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`) VALUES (5,'Kakava Beatos','Kakava',100,'g.',45,19.9);
+INSERT INTO `ingredients` (`idIngredient`,`ingredientName`,`foodCategory`,`portion`,`unit`,`carbsTotalInPortion`,`proteinsTotalInPortion`)  VALUES (6,'Stevia Alvo','Saldiklis',100,'g.',0,0);
